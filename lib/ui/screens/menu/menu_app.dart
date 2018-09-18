5dba6ef54e29ec83a5a8f5db93d2b2532bab227a
Luccas Bikelis <luccasbikelis@peddi.com.br>
2018-09-18 18:44:01 -0300

    botão carrinho, icones superiores prontos!

--- lib/ui/screens/menu/menu_app.dart
+++ lib/ui/screens/menu/menu_app.dart
@@ -3,13 +3,16 @@ import 'package:peddi_tont_app/models/category.dart';
 import 'package:peddi_tont_app/models/sub_category.dart';
 import 'package:peddi_tont_app/themes/app_colors.dart';
 import 'package:peddi_tont_app/models/category.dart';
+import 'package:peddi_tont_app/ui/screens/order/order_app.dart';
 import 'widgets/menu_header.dart';
 import 'widgets/menu_category.dart';
 import 'widgets/menu_sub_category.dart';
+import 'package:peddi_tont_app/themes/font_styles.dart';
 
 class MenuApp extends StatelessWidget {
   final Category selectedCategory;
   final List<Category> storeCategories;
+
   MenuApp(this.selectedCategory, this.storeCategories);
 
   @override
@@ -25,21 +28,56 @@ class MenuApp extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Container(
-//              width: MediaQuery.of(context).size.width,
               height: 130.0,
               color: AppColors.gray1,
               child: MenuHeader(),
             ),
             Expanded(
               child: Container(
-//              width: MediaQuery.of(context).size.width,
-                  child: MenuCategoryTeste(selectedCategory,storeCategories),
-                  height: 300.0,
-                  color: Colors.white),
+                color: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child:
+                          MenuCategoryTeste(selectedCategory, storeCategories),
+                      height: 600.0,
+                      color: Colors.white,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 40.0),
+                        child: new FloatingActionButton.extended(
+                          onPressed: () {
+                            showOrder(context);
+                          },
+                          isExtended: true,
+                          backgroundColor: AppColors.yellow1,
+                          icon: new Icon(
+                            Icons.shopping_cart,
+                            color: AppColors.gray2,
+                            size: 40.0,
+                          ),
+                          label: Text(
+                            'CARRINHO',
+                            style: FontStyles.style2,
+                          ),
+                        ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void showOrder(BuildContext context) {
+    showDialog(context: context, builder: (context) => new OrderApp());
   }
 }
